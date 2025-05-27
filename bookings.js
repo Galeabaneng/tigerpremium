2c05e415499b6092594f5a5db36d799f252fa710
Galeabaneng <133775709+Galeabaneng@users.noreply.github.com>
2025-05-27 22:43:05 +0200

    Update bookings.js

--- bookings.js
+++ bookings.js
@@ -41,3 +41,45 @@ alert("Thank You! An agent will contact you");
 }
 
 
+
+
+function subscribe(){
+
+
+    var email = document.getElementById("emailSub").value;
+
+
+
+
+const secretKey = '$2a$10$pQOgIq2QOg86xvUJzMWNLedwaBnUP2a3qKA9ZWNZzJumuU5FdgABi';
+const binName= 'myBin';
+
+const data = {
+Name: email
+
+
+};
+
+fetch('https://api.jsonbin.io/v3/b', {
+ method: 'POST',
+ headers: {
+   'Content-Type': 'application/json',
+   "X-Master-Key": secretKey,
+   "X-Bin-Private": true,
+   'X-Bin-Name': "NEWSLETTER SUBSCRIPTION"
+ },
+ body: JSON.stringify(data)
+})
+.then(response => response.json())
+.then(jsonData => console.log(jsonData))
+.catch(error => console.error(error));
+
+alert("Thank You!");
+
+}
+
+
+
+
+
+
