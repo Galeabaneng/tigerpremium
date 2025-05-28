50c5769d5034e6d1aab22524abd9a2ac80b977dc
Galeabaneng <133775709+Galeabaneng@users.noreply.github.com>
2025-05-28 12:10:23 +0200

    Update signin.js

--- AdminManager/pages/pages/signin.js
+++ AdminManager/pages/pages/signin.js
@@ -4,7 +4,7 @@ function loginn(){
     var user= document.getElementById("emaill").value;
     var password= document.getElementById("password").value;
     
-    if(user=="Admin" && password=="Adminadmin"){
+    if(user=="tigerpremiumadmn1@gmail.com" && password=="Admin100"){
           window.location.href=("dashboard.html");
     }else{
        alert("Wrong Credentials");
